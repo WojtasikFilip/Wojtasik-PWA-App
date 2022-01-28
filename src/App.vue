d60016323bc4c8180a9879ed7e9451f0e20f693c
Filip <wojtasik.f03@htlwienwest.at>
2022-01-28 21:25:44 +0100

    Appbar mit Logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app>
-    <v-app-bar>
-      
+    <v-app-bar dense dark app>
+      <img src="../public/img/icons/android-chrome-192x192.png" width="40" class="mr-3" />
+      <v-toolbar-title class="font-weight-bold">vData</v-toolbar-title>
+
+      <v-spacer></v-spacer>
     </v-app-bar>
     <v-main>
       <router-view />
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
-  <v-container><h1>Hello</h1></v-container>
+  <v-container>
+    <h1>Endlich!</h1>
+  </v-container>
 </template>
 
 <script>
