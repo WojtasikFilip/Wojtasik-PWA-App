04338cda3f352873234d75ab55698745dcd1fa1d
Filip <wojtasik.f03@htlwienwest.at>
2022-01-28 21:08:34 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container><h1>Hello</h1></v-container>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  transpileDependencies: ['vuetify'],
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'public/service-worker.js',
+    },
+    manifestOptions: {
+      name: 'PWA Demo Employees',
+      short_name: 'PWA Demo',
+      theme_color: '#5e00ff',
+      background_color: '#a16aff',
+      icons: [
+        { src: './img/icons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+        { src: './img/icons/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      ],
+    },
+  },
+};
