bb861d224c3b10f03ff63b5a1c2d2a0648ee47f0
Filip <wojtasik.f03@htlwienwest.at>
2022-02-04 14:59:25 +0100

    test

--- src/router/index.js
+++ src/router/index.js
@@ -13,9 +13,7 @@ const routes = [
   {
     path: '/aufnahme',
     name: 'Aufnahme',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+
     component: () => import('../views/Aufnahme.vue'),
   },
   {
